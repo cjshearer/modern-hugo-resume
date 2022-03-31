2edc019b19ae163d8ce0f279b917c2d98ddb2e68
Cody Shearer <cjshearer@live.com>
2022-03-30 22:12:30 -0400

    feat: added path aliases
    This svelte-ts template from Vite does not contain path aliases to
    common directories, making relative paths the default option when
    importing packages. Because relative paths make refactoring difficult,
    specifically when moving files, I have added path aliases to common
    directories.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      "@assets": fileURLToPath(new URL("src/assets", import.meta.url)),
+      "@lib": fileURLToPath(new URL("src/lib", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("src/lib/components", import.meta.url)
+      ),
+      "@themes": fileURLToPath(new URL("src/lib/themes", import.meta.url)),
+    },
+  },
 });
