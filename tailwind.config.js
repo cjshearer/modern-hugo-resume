b5cfec4b0f8ba9b2b8f083b414804d06c9705164
Cody Shearer <cjshearer@live.com>
2023-09-02 14:24:59 -0400

    feat: add prose styling for markdown
    This commit adds styling to markdown formatted sections using the
    `prose` class provided by `@tailwindcss/typography`, with some slight
    modifications to code blocks and links to better align with the existing
    theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,30 @@
 module.exports = {
   content: ["./hugo_stats.json"],
   plugins: [require("@tailwindcss/typography")],
+  theme: {
+    extend: {
+      typography: () => ({
+        DEFAULT:
+          /** @type {import('tailwindcss').Config['theme']} */
+          {
+            css: {
+              a: {
+                color: false,
+                fontWeight: false,
+                textDecoration: false,
+              },
+              "blockquote p:first-of-type::before": false,
+              "blockquote p:last-of-type::after": false,
+              code: {
+                color: false,
+                fontWeight: false,
+              },
+              "code::after": false,
+              "code::before": false,
+              maxWidth: false,
+            },
+          },
+      }),
+    },
+  },
 };
