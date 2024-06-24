b7d226fcc1f6d0c72cdd32d9b7c7e4da00519d52
Cody Shearer <cjshearer@live.com>
2024-06-24 10:40:25 -0400

    build: improve onboarding (#48)

--- exampleSite/go.mod
+++ exampleSite/go.mod
@@ -2,4 +2,11 @@ module github.com/cjshearer/modern-hugo-resume/exampleSite
 
 go 1.22.3
 
-require github.com/FortAwesome/Font-Awesome v0.0.0-20240402185447-c0f460dca7f7 // indirect
+// We use this for local development. Remove it if you're
+// extracting the exampleSite to your own repository.
+replace github.com/cjshearer/modern-hugo-resume => ../
+
+require (
+	github.com/FortAwesome/Font-Awesome v0.0.0-20240402185447-c0f460dca7f7 // indirect
+	github.com/cjshearer/modern-hugo-resume v0.0.0-20240622205900-d5aff7eecfd0 // indirect
+)
--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         biome = pkgs.biome;
         commitlint = pkgs.commitlint;
+        git = pkgs.git;
         go = pkgs.go;
         hugo = pkgs.hugo;
         nodejs = pkgs.nodejs_22;
@@ -32,8 +33,8 @@
         };
 
         packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
-          pname = "modern-hugo-resume-exampleSite";
-          name = finalAttrs.pname;
+          pname = "modern-hugo-resume-exampleSite"; # remove once I get around to versioning this
+          name = finalAttrs.pname; # remove once I get around to versioning this
 
           src = ./.;
           setSourceRoot = "sourceRoot=$(echo */exampleSite)";
@@ -45,22 +46,34 @@
             hash = "sha256-RvE4R277Kam3s32XbGUIQTToG0cpbhpTaLEU5HsNZZ4=";
           };
 
-          fontAwesome = pkgs.fetchFromGitHub {
-            owner = "FortAwesome";
-            repo = "Font-Awesome";
-            rev = "6.5.2";
-            sha256 = "sha256-kUa/L/Krxb5v8SmtACCSC6CI3qTTOTr4Ss/FMRBlKuw=";
-          };
+          buildPhase =
+            let
+              hugoVendor = pkgs.stdenv.mkDerivation {
+                name = "${finalAttrs.pname}-hugoVendor";
+                inherit (finalAttrs) src setSourceRoot;
+                nativeBuildInputs = [ go hugo git ];
 
-          postBuild = ''
-            go mod vendor
-            mv vendor _vendor
-            sed -i '$d' _vendor/modules.txt
-            mkdir -p _vendor/github.com/FortAwesome/
-            ln -s $fontAwesome _vendor/github.com/FortAwesome/Font-Awesome
+                buildPhase = ''
+                  hugo mod vendor
+                '';
 
-            pnpm build -d $out
-          '';
+                installPhase = ''
+                  cp -r _vendor $out
+                '';
+
+                outputHashMode = "recursive";
+                outputHashAlgo = "sha256";
+                # To get a new hash:
+                # 1. Invalidate the current hash (change any character between "sha256-" and "=")
+                # 2. Run`nix build` (it will fail with a hash-mismatch and provide the actual hash)
+                # 3. Substitute the correct hash (`nix build` should now work)
+                outputHash = "sha256-szsB5HwBznoZ1+qtj/yGgDQeUJxVdgtJ/4O1I25s4UE=";
+              };
+            in
+            ''
+              ln -s ${hugoVendor} _vendor
+              pnpm build -d $out
+            '';
 
           dontInstall = true;
           dontFixup = true;
