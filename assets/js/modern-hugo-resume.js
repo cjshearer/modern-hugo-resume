0411d2cf6ef0de29241aec6a7aeb589caf875b98
Cody Shearer <cjshearer@live.com>
2024-07-29 14:49:09 -0400

    fix: footer positioning (#60)
    * fix: automatic page count for small screens
    
    * fix: automatic footer positioning for print
    
    * feat: use paged.js for print layout

---
+++ assets/js/modern-hugo-resume.js
@@ -0,0 +1,51 @@
+async function initializePagedJS() {
+  await Promise.all([
+    import("https://unpkg.com/pagedjs@0.4.3/dist/paged.min.js"),
+    new Promise((resolve) => {
+      if (document.readyState !== "loading") resolve();
+      document.addEventListener("DOMContentLoaded", () => resolve());
+    }),
+  ]);
+
+  // Workaround for using custom properties with @page rules. For background on why this is an
+  // issue, see: https://stackoverflow.com/a/44738574
+  const referencePage = document.createElement("div");
+  referencePage.style.height = "var(--page-height)";
+  referencePage.style.width = "var(--page-width)";
+  referencePage.style.margin = "var(--page-margin)";
+  referencePage.style.position = "absolute";
+  referencePage.style.top = "-9999px";
+  document.body.appendChild(referencePage);
+  const referencePageStyles = window.getComputedStyle(referencePage);
+  const style = document.createElement("style");
+  style.innerHTML = `@page {
+    ${Object.entries({
+      size: `${referencePageStyles.width} ${referencePageStyles.height}`,
+      margin: referencePageStyles.margin,
+    }).reduce((acc, [key, value]) => `${acc}${key}: ${value};\n`, "")}
+  }`;
+  document.head.appendChild(style);
+  document.body.removeChild(referencePage);
+
+  const previewer = new PagedModule.Previewer();
+  await previewer.preview();
+}
+
+function showPrintPage() {
+  const url = new URL(window.location.href);
+  url.searchParams.set("print", "true");
+  history.pushState({}, "", url);
+  initializePagedJS();
+}
+
+if (new URL(window.location.href).searchParams.has("print")) {
+  initializePagedJS();
+} else {
+  window.onbeforeprint = showPrintPage;
+}
+
+window.onafterprint = () => {
+  const url = new URL(window.location.href);
+  url.searchParams.delete("print");
+  window.location.href = url.href;
+};
