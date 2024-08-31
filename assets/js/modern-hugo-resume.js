44dff7036b96cc6de30258fc44d4e32919d87129
cjshearer <cjshearer@live.com>
2024-08-31 14:20:11 -0400

    refactor!: simplify page params

--- assets/js/modern-hugo-resume.js
+++ assets/js/modern-hugo-resume.js
@@ -1,34 +1,10 @@
-const pagedJS = "https://unpkg.com/pagedjs@0.4.3/dist/paged.min.js";
-import(pagedJS);
+import "https://unpkg.com/pagedjs@0.4.3/dist/paged.min.js";
 
 async function initializePagedJS() {
-  await Promise.all([
-    import(pagedJS),
-    new Promise((resolve) => {
-      if (document.readyState !== "loading") resolve();
-      document.addEventListener("DOMContentLoaded", () => resolve());
-    }),
-  ]);
-
-  // Workaround for using custom properties with @page rules. For background on why this is an
-  // issue, see: https://stackoverflow.com/a/44738574
-  const referencePage = document.createElement("div");
-  referencePage.style.height = "var(--page-height)";
-  referencePage.style.width = "var(--page-width)";
-  referencePage.style.margin = "var(--page-margin)";
-  referencePage.style.position = "absolute";
-  referencePage.style.top = "-9999px";
-  document.body.appendChild(referencePage);
-  const referencePageStyles = window.getComputedStyle(referencePage);
-  const style = document.createElement("style");
-  style.innerHTML = `@page {
-    ${Object.entries({
-      size: `${referencePageStyles.width} ${referencePageStyles.height}`,
-      margin: referencePageStyles.margin,
-    }).reduce((acc, [key, value]) => `${acc}${key}: ${value};\n`, "")}
-  }`;
-  document.head.appendChild(style);
-  document.body.removeChild(referencePage);
+  await new Promise((resolve) => {
+    if (document.readyState !== "loading") resolve();
+    document.addEventListener("DOMContentLoaded", () => resolve());
+  });
 
   const previewer = new PagedModule.Previewer();
   await previewer.preview();
